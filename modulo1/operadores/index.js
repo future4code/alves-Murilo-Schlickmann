e390ca903c5ea3e168c322bab8b918789639db9d
Murilo Voigt Schlickmann <murilo.praesoft@gmail.com>
2022-05-10 14:11:50 -0300

    ajustes-técnicos_kkkkk

--- modulo1/operadores/index.js
+++ modulo1/operadores/index.js
@@ -1,6 +1,10 @@
-const a = prompt="Digite sua idade:"
-const b = prompt="Digite a idade de seu melhor amigo"
+const a =prompt="Digite sua idade:"
+const b =prompt="Digite a idade de seu melhor amigo"
+
 console.log(a - b)
+
 const resultado = a - b
 console.log(resultado)
+
 let result = a >= b
+console.log(result)
