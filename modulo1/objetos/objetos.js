454075a12a07bb111d5efbf1be1d8cc5496f53c4
Murilo Voigt Schlickmann <murilo.praesoft@gmail.com>
2022-05-16 17:31:33 -0300

    Aula-Objetos

---
+++ modulo1/objetos/objetos.js
@@ -0,0 +1,74 @@
+//EX-01
+//a)
+// const pessoa = {
+//     nome:"Murilo",
+//     apelidos:"Muh, Mumu, Murilão"
+// }
+// //b)
+// const novosApelidos=["Murilex", pessoa.apelidos, "Alemãozinho"]
+// console.log(`Eu sou ${pessoa.nome}, mas pode chamar de: ${novosApelidos}`)
+
+//EX-02
+// let array = []
+// array.push
+// array.push
+// console.log(array);
+// const pessoa1 = {
+//     nome1: "Nivaldo Schlickmann",
+//     idade1: "57",
+//     profissao1:"Contador",
+// }
+// console.log(`Eu sou ${pessoa1.nome1}, tenho ${pessoa1.idade1} anos, trabalho como ${pessoa1.profissao1}`)
+
+// const pessoa2 = {
+//     nome2: "Loni Voigt",
+//     idade2:"55",
+//     profissao2:"Vendedora"
+// }
+// console.log(`Eu sou ${pessoa2.nome2}, tenho ${pessoa2.idade2} e trabalho como ${pessoa2.profissao2}`)
+// const pessoa3 = {
+//     nome3:"Murilo Voigt Schlickmann",
+//     idade3:"20",
+//     profissao3:"Suporte e Estudante"
+// }
+// console.log(`Eu sou o ${pessoa3.nome3}, tenho${pessoa3.idade3} e trabalho como ${pessoa3.profissao3}`)
+// let array1 = [];
+// array1.length [pessoa3.nome3]
+// array1.length [pessoa3.idade3]
+// array1.length [pessoa3.profissao3]
+
+//EX-03
+// let array = [];
+// array.push({id: 1, nome:`mamao`})
+// array.push({id: 2, nome: `pera`})
+// array.push({id: 3, nome: `maca`})
+// console.log(array)
+// let fruta1 = {
+//     nome: "mamao",
+//     disponibilidade: true
+// }
+// let fruta2 = {
+//     nome: "pera",
+//     disponibilidade1: true
+// }
+// let fruta3 = {
+//     nome:"maca",
+//     disponibilidade2: true
+// }
+
+// function adicionarFruta(fruta1, fruta2, fruta3){
+//     const array = [fruta1, fruta2, fruta3]
+//     return array
+// }
+
+// carrinho = adicionarFruta()
+
+// function controlarEstoque(frutas){
+//     const controleEstoque = !fruta1.disponibilidade
+//     const controleEstoque1 = !fruta2.disponibilidade
+//     const controleEstoque2 = !fruta3.disponibilidade
+//     console.log(controleEstoque)
+//     console.log(controleEstoque1)
+//     console.log(controleEstoque2)    
+//     return controleEstoque, controleEstoque1, controleEstoque2
+// }
