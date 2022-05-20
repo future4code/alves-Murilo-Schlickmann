d3aeeced7e0b36ed6a038a9b0c2b0d15157eb71a
Murilo Voigt Schlickmann <murilo.praesoft@gmail.com>
2022-05-20 16:38:17 -0300

    Revisao-js

---
+++ modulo1/revisao-js-postinho/revisao.js
@@ -0,0 +1,51 @@
+//EX-01
+//A)
+/*function checarDesigualdade(a, b) {
+
+      return `No comparador de desigualdade ${a}!==${b} é ${a !== b}`
+    
+    }
+    
+    console.log(checarDesigualdade(1, 2));*/
+//b)
+/*function checarDesigualdade(a, b) {
+
+    return `No comparador de desigualdade ${a}!==${b} é ${a !== b}`
+  
+  }
+  
+  console.log(checarDesigualdade(1, 1))
+  console.log(checarDesigualdade(5, 1))
+  console.log(checarDesigualdade(3, 1))
+  console.log(checarDesigualdade(2, 2))*/
+  //C)
+  /*function verificaSeEMaior(a, b) {
+
+    return `Na verificação se é maior ${a} > ${b} é ${a > b}`
+  
+  }
+  
+  console.log(verificaSeEMaior(1, 1))
+  console.log(verificaSeEMaior(5, 1))
+  console.log(verificaSeEMaior(3, 1))
+  console.log(verificaSeEMaior(2, 2))*/
+
+//EX-02
+    /*A) True
+      B) True
+      C) false
+      D) false
+      E) true*/
+//EX-03
+/*nomeDoUsuario = prompt('Digite seu nome de usuario')
+anoDeNascimento = prompt('Digite o ano do seu nascimento') 
+senhaDoUsuario = prompt('Digite sua senha')
+nacionalidade = prompt('Digite sua nacionalidade')
+$('#anoDeNascimento').change(function() {
+  dt = $('#dataNascimento').val();
+  dt = dt.split('/').reverse().join('/');
+  dob = new Date(dt);
+  var today = new Date();
+  var age = Math.floor((today-dob) / (365.25 * 24 * 60 * 60 * 1000));
+  alert(age+' years old'); 
+})*/
