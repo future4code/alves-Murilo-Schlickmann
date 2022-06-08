a49a65f0bf49ef01966b800f02c0f460255c4867
Murilo Voigt Schlickmann <murilo.praesoft@gmail.com>
2022-06-08 16:48:44 -0300

    estados-insta4

---
+++ modulo-2/ClassComponent/teste/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <a
+          href="https://praesoft.com.br/"
+        >Prae</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
